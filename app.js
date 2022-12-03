93320ba5d49994e37405c289022bdf299e765957
kmfiguerrez <kmfiguerrez@gmail.com>
2022-12-03 17:49:39 +0800

    Add img directtory and edit about me section

--- app.js
+++ app.js
@@ -7,46 +7,18 @@ const swc = document.getElementById("second-word");
 // const toDisplay = "JAVASCRIPT DEVELOPER";
 // jtc means job title container.
 const jtc = document.getElementById("job-title");
-const greeting = document.getElementById("greeting");
 const jobTitles = ["IT SPECIALIST", "WEB DEVELOPER"];
 
-
-let stopDisplay = false;
-
-greeting.addEventListener("click", () => {
-    stopDisplay = true;
-})
-
-// const display = async () => {
-//     for (const job of jobTitles) {
-//         console.log(job)
-//         const dRes = await util.displayText(job, fwc, swc);
-//         console.log(dRes);
-//         const sRes = await util.sleep(3000);
-//         console.log(sRes);        
-//         if (job === jobTitles[jobTitles.length - 1]) {
-//             console.log("ayo!")
-//             break;
-//         }
-//         const rRes = await util.removeText(fwc, swc);
-//         console.log(rRes);        
-//     }        
-// }
-
 const display = async () => {
-    while (!stopDisplay) {
-        for (const job of jobTitles) {
-            
+    while (true) {
+        for (const job of jobTitles) {            
             // console.log(job)
             const dRes = await util.displayText(job, fwc, swc);
             // console.log(dRes);
             const sRes = await util.sleep(3000);
             // console.log(sRes);        
             const rRes = await util.removeText(fwc, swc);
-            // console.log(rRes);
-            if (stopDisplay) {
-                break;
-            }        
+            // console.log(rRes);                    
         }    
     }            
 }
